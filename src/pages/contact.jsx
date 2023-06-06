d38f9a555fa6d1a15d971d085522fdc45c303385
Yaroslav Oliinyk <4106677@gmail.com>
2023-06-06 08:59:51 +0300

    add contacts section

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,17 @@
+import Contact from "@/components/Contact/Contact";
+import Hero from "@/components/Hero/Hero";
 import React from "react";
 
 const contact = () => {
-  return <div>contact</div>;
+  return (
+    <div>
+      <Hero
+        heading="Contact"
+        message="Submit fhe form below for more work quotes."
+      />
+      <Contact />
+    </div>
+  );
 };
 
 export default contact;
